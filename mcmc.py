6bd8af4a02d66c0e631beff3815dbf49a5c1679f
Robert McGill <robertmcgill@myway.com>
2015-10-29 15:13:29 +0100

    Added useable_ace function

--- mcmc.py
+++ mcmc.py
@@ -7,3 +7,10 @@ def randomCard():
     if card > 10:
         card = 10
     return card
+
+# A hand is just a tuple.
+# Ex.: (14, False), a total card value of 14 without a useable ace
+# If the Ace can be an 11 without busting the hand, it's useable
+def useable_ace(hand):
+    val, ace = hand
+    return ((ace) and ((val + 10) <= 21))
